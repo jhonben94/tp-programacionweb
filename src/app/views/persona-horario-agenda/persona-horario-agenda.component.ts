501e6d3a3c3190d83b6a2ff718a5e0e62faff2d9
Jhony Benitez <88219328+qrsense@users.noreply.github.com>
2021-09-22 13:44:22 -0400

    Modificaciones para funcionalidades.

--- src/app/views/persona-horario-agenda/persona-horario-agenda.component.ts
+++ src/app/views/persona-horario-agenda/persona-horario-agenda.component.ts
@@ -15,10 +15,10 @@ import { PersonaHorarioAgendaService } from "src/app/services";
 import { MatDialog } from "@angular/material/dialog";
 
 import swal from "sweetalert2";
-import {PersonaHorarioAgendaEditComponent} from './persona-horario-agenda-edit/persona-horario-agenda-edit.component';
-import {Router} from '@angular/router';
-import {BuscadorEmpleadoComponent} from '../buscadores/buscador-empleado/buscador-empleado.component';
-import {BuscadorClienteComponent} from '../buscadores/buscador-cliente/buscador-cliente.component';
+import { PersonaHorarioAgendaEditComponent } from "./persona-horario-agenda-edit/persona-horario-agenda-edit.component";
+import { Router } from "@angular/router";
+import { BuscadorEmpleadoComponent } from "../buscadores/buscador-empleado/buscador-empleado.component";
+import { BuscadorClienteComponent } from "../buscadores/buscador-cliente/buscador-cliente.component";
 
 @Component({
   selector: "app-persona-horario-agenda",
@@ -108,27 +108,27 @@ export class PersonaHorarioAgendaComponent implements OnInit {
       relacion: true,
       descripcion: "EMPLEADO",
       columnaRelacion: ["nombre", "apellido"],
-    }
+    },
   ];
   /**
    * @type {Array}
    * @description Lista que contiene los valores para la grilla
    */
   data: any[] = [];
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
   listaSemana: { codigo: number; valor: string }[];
 
   constructor(
-      private fb: FormBuilder,
-      private service: PersonaHorarioAgendaService,
-      public dialog: MatDialog,
-      private router: Router
+    private fb: FormBuilder,
+    private service: PersonaHorarioAgendaService,
+    public dialog: MatDialog,
+    private router: Router
   ) {
     this.filtrosForm = this.fb.group({
       dia: [""],
       idEmpleado: [""],
-      nombreEmpleado: [""]
+      nombreEmpleado: [""],
     });
   }
 
@@ -149,37 +149,37 @@ export class PersonaHorarioAgendaComponent implements OnInit {
 
   buscar() {
     merge(this.sort.sortChange, this.paginator.page)
-        .pipe(
-            startWith({}),
-            switchMap(() => {
-              this.isLoadingResults = true;
-                let filterData = this.filtrosForm.value;
-                delete filterData.nombreEmpleado;
-              const params = {
-                cantidad: this.paginator.pageSize,
-                inicio: this.retornaInicio(),
-                orderBy: this.sort.active,
-                orderDir: this.sort.direction,
-                like: "S",
-                ejemplo: JSON.stringify(deleteEmptyData(this.filtrosForm.value)),
-              };
-              return this.service.listarRecurso(params);
-            }),
-            map((data: any) => {
-              // Flip flag to show that loading has finished.
-              this.isLoadingResults = false;
-              this.isRateLimitReached = false;
-              this.resultsLength = data.totalDatos;
-              return data.lista;
-            }),
-            catchError(() => {
-              this.isLoadingResults = false;
-              // Catch if the API has reached its rate limit. Return empty data.
-              this.isRateLimitReached = true;
-              return of([]);
-            })
-        )
-        .subscribe((data) => (this.data = data));
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          this.isLoadingResults = true;
+          let filterData = this.filtrosForm.value;
+          delete filterData.nombreEmpleado;
+          const params = {
+            cantidad: this.paginator.pageSize,
+            inicio: this.retornaInicio(),
+            orderBy: this.sort.active,
+            orderDir: this.sort.direction,
+            like: "S",
+            ejemplo: JSON.stringify(deleteEmptyData(this.filtrosForm.value)),
+          };
+          return this.service.listarRecurso(params);
+        }),
+        map((data: any) => {
+          // Flip flag to show that loading has finished.
+          this.isLoadingResults = false;
+          this.isRateLimitReached = false;
+          this.resultsLength = data.totalDatos;
+          return data.lista;
+        }),
+        catchError(() => {
+          this.isLoadingResults = false;
+          // Catch if the API has reached its rate limit. Return empty data.
+          this.isRateLimitReached = true;
+          return of([]);
+        })
+      )
+      .subscribe((data) => (this.data = data));
   }
 
   agregar(): void {
@@ -193,37 +193,37 @@ export class PersonaHorarioAgendaComponent implements OnInit {
         break;
       case "eliminar":
         swal
-            .fire({
-              title: "Está seguro que desea eliminar el registro?",
-              text: "Esta acción no se podrá revertir!",
-              icon: "warning",
-              showCancelButton: true,
-              customClass: {
-                confirmButton: "btn btn-success",
-                cancelButton: "btn btn-danger",
-              },
-              confirmButtonText: "Eliminar",
-              buttonsStyling: false,
-            })
-            .then((result) => {
-              if (result.value) {
-                this.service.eliminarRecurso(data[id]).subscribe((res) => {
-                  swal
-                      .fire({
-                        title: "Éxito!",
-                        text: "El registro fue eliminado correctamente.",
-                        icon: "success",
-                        customClass: {
-                          confirmButton: "btn btn-success",
-                        },
-                        buttonsStyling: false,
-                      })
-                      .then(() => {
-                        this.limpiar();
-                      });
-                });
-              }
-            });
+          .fire({
+            title: "Está seguro que desea eliminar el registro?",
+            text: "Esta acción no se podrá revertir!",
+            icon: "warning",
+            showCancelButton: true,
+            customClass: {
+              confirmButton: "btn btn-success",
+              cancelButton: "btn btn-danger",
+            },
+            confirmButtonText: "Eliminar",
+            buttonsStyling: false,
+          })
+          .then((result) => {
+            if (result.value) {
+              this.service.eliminarRecurso(data[id]).subscribe((res) => {
+                swal
+                  .fire({
+                    title: "Éxito!",
+                    text: "El registro fue eliminado correctamente.",
+                    icon: "success",
+                    customClass: {
+                      confirmButton: "btn btn-success",
+                    },
+                    buttonsStyling: false,
+                  })
+                  .then(() => {
+                    this.limpiar();
+                  });
+              });
+            }
+          });
         break;
       case "editar":
         this.router.navigate(["persona-horario-agenda/modificar/", data[id]]);
@@ -233,18 +233,23 @@ export class PersonaHorarioAgendaComponent implements OnInit {
     }
   }
 
-
   mostrarCampo(row, columna) {
     if (columna.relacion) {
       if (row[columna.label] == null) {
         return "";
       }
+      if (Array.isArray(columna.columnaRelacion)) {
+        return this.multipleColumnas(
+          row[columna.label],
+          columna.columnaRelacion
+        );
+      }
       return row[columna.label][columna.columnaRelacion];
     } else {
       if (typeof columna.estados != "undefined") {
         const label = row[columna.label]
-            ? columna.estados[0]
-            : columna.estados[1];
+          ? columna.estados[0]
+          : columna.estados[1];
         return label;
       }
       if (columna.label == "dia") {
@@ -252,6 +257,14 @@ export class PersonaHorarioAgendaComponent implements OnInit {
       }
       return row[columna.label];
     }
+  }
+  multipleColumnas(valor: any, listaCol: any[]) {
+    let valorRetorno = "";
+    for (let index = 0; index < listaCol.length; index++) {
+      const property = listaCol[index];
+      valorRetorno += valor[property] + " ";
+    }
+    return valorRetorno;
   }
 
   limpiar() {
@@ -265,8 +278,8 @@ export class PersonaHorarioAgendaComponent implements OnInit {
 
     if (this.paginator.pageIndex > 0) {
       return (
-          cantidad *
-          (0 == this.paginator.pageIndex ? 1 : this.paginator.pageIndex)
+        cantidad *
+        (0 == this.paginator.pageIndex ? 1 : this.paginator.pageIndex)
       );
     }
     return inicio;
@@ -286,7 +299,7 @@ export class PersonaHorarioAgendaComponent implements OnInit {
           console.log(result);
           if (result) {
             this.f.nombreEmpleado.setValue(
-                result.nombre + " " + result.apellido
+              result.nombre + " " + result.apellido
             );
             this.f.idEmpleado.setValue(result.idPersona);
           } else {
@@ -306,7 +319,7 @@ export class PersonaHorarioAgendaComponent implements OnInit {
           console.log(result);
           if (result) {
             this.f.nombreCliente.setValue(
-                result.nombre + " " + result.apellido
+              result.nombre + " " + result.apellido
             );
             this.f.idCliente.setValue(result.idPersona);
           } else {
